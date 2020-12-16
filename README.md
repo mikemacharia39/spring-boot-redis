ace07e8e47f5261e74027586c184307f7e633f81
mikemacharia39 <mikemacharia39@gmail.com>
2020-12-17 00:35:42 +0300

    Updated readme.md

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# spring-boot-redis
+# Spring Boot Redis
 This project shows how to work with redis
+
+### Getting Started
+
+* The application shows how to work with Redis
+* Ensure you have installed the below applications
+
+    |Application|Version|
+    |---------|------------|
+    |Redis||
+    |JDK| \> 11 |
+
+
+### Overview
+The application uses 3 mechanisms to work with Redis:
+1. HashOperations
+2. ValueOperations
+3. JPA - Working with persistence methods
+
+Either of the above will work and usage depends on the context
+
+### Setup
+
+1. git clone https://github.com/mikemacharia39/spring-boot-redis.git
+
+2. mvn clean install package 
+
+### Testing
+* Postman Collection
+https://www.getpostman.com/collections/3c373b9609b6a280cdc3
--- src/main/java/com/mikehenry/springbootredis/configuration/RedisConfiguration.java
+++ src/main/java/com/mikehenry/springbootredis/configuration/RedisConfiguration.java
@@ -1,7 +1,9 @@
 package com.mikehenry.springbootredis.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
@@ -10,9 +12,21 @@ import org.springframework.data.redis.core.RedisTemplate;
 @Configuration
 public class RedisConfiguration {
 
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Value("${spring.redis.password}")
+    private String password;
+
     @Bean
     JedisConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration("127.0.0.1", 6379);
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+        redisConfig.setHostName(this.host);
+        redisConfig.setPort(this.port);
+        redisConfig.setPassword(RedisPassword.of(this.password));
         JedisClientConfiguration jedisClientConfiguration = JedisClientConfiguration.builder().usePooling().build();
         JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(redisConfig, jedisClientConfiguration);
         jedisConnectionFactory.afterPropertiesSet();
