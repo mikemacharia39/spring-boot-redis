0c106691c02076e1c950c1b026930913a9157070
mikemacharia39 <mikemacharia39@gmail.com>
2025-01-15 23:20:20 +0300

    cleanup use of redis hash functions

--- src/main/java/com/mikehenry/springbootredis/controller/EmployeeAddressController.java
+++ src/main/java/com/mikehenry/springbootredis/controller/EmployeeAddressController.java
@@ -5,19 +5,31 @@ import com.mikehenry.springbootredis.requests.CreateAddressRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * The idea of behind this EmployeeAddressController and EmployeeHashingRepository is to demonstrate how to store, retrieve and delete using
+ * Redis Hashing data structure. The EmployeeHashingRepository is a custom repository that extends RedisRepository and provides methods to
+ * store, retrieve and delete data using Redis Hashing data structure.
+ */
+
 @RestController
-@RequestMapping("/api/v1/employeeAddress/")
+@RequestMapping("/employeeAddress")
 public class EmployeeAddressController {
 
     @Autowired
     EmployeeHashingRepository employeeHashingRepository;
 
-    @PostMapping("create")
+    @PostMapping
     public ResponseEntity<Object> createEmployeeByHash(@RequestBody CreateAddressRequest createAddressRequest) {
 
         String msisdn = createAddressRequest.getMobileNumber();
@@ -38,17 +50,28 @@ public class EmployeeAddressController {
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
-    @GetMapping("getEmployeeAddress/{msisdn}")
+    @GetMapping("/{msisdn}")
     public ResponseEntity<Object> getAddressByMSISDN(@PathVariable String msisdn) {
 
-        Map dataMap = employeeHashingRepository.getUserAddress(msisdn);
+        Map dataMap = employeeHashingRepository.getAddressByMobileNumber(msisdn);
 
         Map<String, Object> response = new HashMap<>();
         response.put("statusCode", 1);
         response.put("message", "Successfully saved address");
         response.put("data", dataMap);
 
-        return new ResponseEntity<>(response, HttpStatus.CREATED);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+    @DeleteMapping("/{msisdn}")
+    public ResponseEntity<Object> deleteAddressByMSISDN(@PathVariable String msisdn) {
+
+        employeeHashingRepository.delete(msisdn);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("statusCode", 1);
+        response.put("message", "Successfully deleted address");
+
+        return new ResponseEntity<>(response, HttpStatus.NO_CONTENT);
+    }
 }
